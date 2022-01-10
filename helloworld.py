3cad08038f42671fbbc1ed13962b23f15c9ee76d
GeryNi <78086592+GeryNi@users.noreply.github.com>
2022-01-10 15:58:38 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -5,6 +5,9 @@ outputLocation = sys.argv[2]
 print("Calculating 2 * ",value)
 result = 2 * value
 
+print("Writing to file ",outputLocation)
 file = open(outputLocation,'w') 
 file.write(result)
 file.close()
+
+print("Terminated successfully")
