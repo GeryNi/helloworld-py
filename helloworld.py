21f1788d0e4d7c4cd7b0f7582d9a0071ec0c3a41
GeryNi <78086592+GeryNi@users.noreply.github.com>
2022-01-10 10:45:50 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,10 @@
 import sys
 
 value = sys.argv[1]
+outputLocation = sys.argv[2]
 print("Calculating 2 * ",value)
 result = 2 * value
 
-file = open('result.txt','w') 
+file = open(outputLocation,'w') 
 file.write(result)
 file.close()
