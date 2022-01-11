781e5db43294ea55d5150b9748410f1c286a540f
GeryNi <78086592+GeryNi@users.noreply.github.com>
2022-01-11 13:14:41 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,16 @@
-print("Hello World !")
+import sys
+import numpy as np
+
+print("Numpy version: ", np.__version__)
+
+value = sys.argv[1]
+outputLocation = sys.argv[2]
+print("Calculating 2 * ",value)
+result = 2 * value
+
+print("Writing to file ",outputLocation)
+file = open(outputLocation,'w') 
+file.write(result)
+file.close()
+
+print("Terminated successfully")
